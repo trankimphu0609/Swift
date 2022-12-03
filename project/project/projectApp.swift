4e10f9444debb348debb13888cc5c33d56659c1f
Trần Kim Phú <trankimphu0609@gmail.com>
2022-12-03 21:09:27 +0700

    demo code

---
+++ project/project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Trần Kim Phú on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
